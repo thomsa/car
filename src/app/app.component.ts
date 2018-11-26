897fc9344ddf6ad4cb672572afc0ad1610bf774d
Tamas Lorincz <tamas.lorincz@solarwinds.com>
2018-11-26 12:49:06 +0100

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'car-runner';
+  title = "car-runner";
+  ignited = false;
+  isRunning = false;
+  gas = 0;
+  oil = 0;
+  coolingLiquid = 0;
+  isKeyIn = false;
+
+  runningInterval;
+  ignite() {
+    if (
+      !this.isKeyIn ||
+      this.gas === 0 ||
+      this.oil === 0 ||
+      this.coolingLiquid === 0
+    ) {
+      alert("This car can't start! Probably there's no gas or oil!");
+    } else {
+      this.ignited = true;
+    }
+  }
+
+  start() {
+    if (this.ignited) {
+      this.isRunning = true;
+    }
+    this.runningInterval = setInterval(() => {
+      if (this.isRunning) {
+        this.gas = this.gas - 1;
+        this.oil = this.oil - 0.01;
+      }
+    }, 1000);
+  }
+  stop() {
+    this.runningInterval = undefined;
+    this.isRunning = false;
+  }
 }
